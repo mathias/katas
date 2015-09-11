c5079104c2d6a1156f6c8a734718b6473ac9a520
Matt Gauger <matt.gauger@gmail.com>
2015-09-11 18:59:37 -0500

    Get the first number in our list greater than current prime
    - If it is nil, that means we're done -- there's nothing left in the list to sieve.
    - If it isn't nil, we need to recur and keep sieving

--- eratosthenes-sieve/src/eratosthenes_sieve/core.clj
+++ eratosthenes-sieve/src/eratosthenes_sieve/core.clj
@@ -6,12 +6,15 @@
 (defn- multiple-of? [n prime]
   (and (zero? (mod n prime)) (not= n prime)))
 
+(defn- next-prime [last-prime numbers]
+  (first (filter #(> % last-prime) numbers)))
+
 (defn- sieve [numbers prime]
   (remove #(multiple-of? % prime) numbers))
 
 (defn primes-up-to [n]
   (loop [numbers (all-integers-up-to n)
-         current (first numbers)]
-    (if (= current n)
+         prime 2]
+    (if (nil? prime)
       numbers
-      (recur (sieve numbers current) (inc current)))))
+      (recur (sieve numbers prime) (next-prime prime numbers)))))
