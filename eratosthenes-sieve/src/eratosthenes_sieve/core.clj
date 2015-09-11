eee4f9f98b9a6a777c040d055c1334ad803c29f9
Matt Gauger <matt.gauger@gmail.com>
2015-09-11 18:31:42 -0500

    Refactor out a function

--- eratosthenes-sieve/src/eratosthenes_sieve/core.clj
+++ eratosthenes-sieve/src/eratosthenes_sieve/core.clj
@@ -1,6 +1,9 @@
 (ns eratosthenes-sieve.core)
 
+(defn- all-integers-up-to [n]
+  (range 2 (inc n)))
+
 (defn primes-up-to [n]
   (filter #(or (not= 0 (mod % 2))
                (= 2 %))
-   (range 2 (inc n))))
+          (all-integers-up-to n)))
