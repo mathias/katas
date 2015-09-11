c003365853c60b4a6c8ba2dc5c69248ed47bd5b8
Matt Gauger <matt.gauger@gmail.com>
2015-09-11 18:38:11 -0500

    Here we can begin to see the recursive structure needed

--- eratosthenes-sieve/src/eratosthenes_sieve/core.clj
+++ eratosthenes-sieve/src/eratosthenes_sieve/core.clj
@@ -3,9 +3,13 @@
 (defn- all-integers-up-to [n]
   (range 2 (inc n)))
 
-(defn- multiple-of? [n p]
-  (and (zero? (mod n p)) (not= n p)))
+(defn- multiple-of? [n prime]
+  (and (zero? (mod n prime)) (not= n prime)))
+
+(defn- sieve [numbers prime]
+  (remove #(multiple-of? % prime) numbers))
 
 (defn primes-up-to [n]
-  (filter #(multiple-of? % 2)
-          (all-integers-up-to n)))
+  (sieve
+   (sieve (all-integers-up-to n) 2)
+   3))
