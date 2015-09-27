f360b3d2cf2c59aa3c8b21866082a39440eded23
Matt Gauger <matt.gauger@gmail.com>
2015-09-27 17:15:42 -0500

    Pass slightly more complex no-spaces test

--- word-wrap/test/word_wrap/core_test.clj
+++ word-wrap/test/word_wrap/core_test.clj
@@ -8,4 +8,6 @@
        (fact "Text that is less than the requested cols does not wrap"
              (wrap "this" 10) => "this")
        (fact "Wrap single word when requested cols is less than its length"
-             (wrap "word" 2) => "wo\nrd"))
+             (wrap "word" 2) => "wo\nrd")
+       (fact "Wrap string without spaces that will wrap multiple times"
+             (wrap "abcdefghij" 3) => "abc\ndef\nghi\nj"))
