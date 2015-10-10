0f01ee55e2662089c2a84e874b47f69c405178fb
Matt Gauger <matt.gauger@gmail.com>
2015-10-10 15:02:14 -0500

    Implementing all-numbers first made implementing includes? easier

--- integer-ranges/src/integer_ranges/core.clj
+++ integer-ranges/src/integer_ranges/core.clj
@@ -1,4 +1,29 @@
 (ns integer-ranges.core)
 
-(defn includes? [rangestr includestr]
-  true)
+(defn- parse-interval [interval]
+  (let [matched (map first (re-seq #"([\[\(\]\)]{1}|\d+)" interval))
+        start-operator (ffirst matched)
+        start-number (Integer. (second matched))
+        end-number (Integer. (nth matched 2)) ;; 0-indexed vector
+        end-operator (first (last matched))]
+    [(if (= \[ start-operator)
+       start-number
+       (inc start-number))
+     (if (= \) end-operator)
+       end-number
+       (inc end-number))]))
+
+(defn- parse-numbers [numbers]
+  (->> numbers
+       (re-seq #"(\d+)")
+       (map first)
+       (map #(Integer. %))))
+
+(defn all-numbers [interval]
+  (let [[start end] (parse-interval interval)]
+    (range start end)))
+
+(defn includes? [interval includes]
+  (let [interval-range (all-numbers interval)
+        numbers (parse-numbers includes)]
+    (every? #(contains? (set interval-range) %) numbers)))
--- integer-ranges/test/integer_ranges/core_test.clj
+++ integer-ranges/test/integer_ranges/core_test.clj
@@ -3,7 +3,13 @@
             [integer-ranges.core :refer :all]))
 
 (facts "about integer ranges"
-
        (fact
-        "it knows which numbers an range includes"
-        (includes? "[2, 5]" "{2,3,4,5}") => true))
+        "it knows which numbers a range includes"
+        (includes? "[2, 5]" "{2,3,4,5}") => true
+        (includes? "[2, 5]" "{2,-1}") => false)
+       (fact
+        "all-numbers gives all numbers in interval"
+        (all-numbers "[2,6]") => [2 3 4 5 6]
+        (all-numbers "[1, 5]") => [1 2 3 4 5]
+        (all-numbers "(1,5]") => [2 3 4 5]
+        (all-numbers "(1,5)") => [2 3 4]))
